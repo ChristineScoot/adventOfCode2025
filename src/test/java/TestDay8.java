c9179676d5971ede4ada7cb6bc80d2aceaff6042
ChristineScoot <skotnicznyk@gmail.com>
2025-12-10 19:38:20 +0100

    refactor

--- src/test/java/TestDay8.java
+++ src/test/java/TestDay8.java
@@ -14,20 +14,8 @@ public class TestDay8 {
     }
 
     @Test
-    public void part1Sol() throws Exception {
-        Method method = Class.forName("Solutions.Day" + day).getMethod("part1", String.class, int.class);
-        assertEquals(131150L, method.invoke(null, String.format("src/main/resources/input%d_1.txt", day), 1000));
-    }
-
-    @Test
     public void part2() throws Exception {
         Method method = Class.forName("Solutions.Day" + day).getMethod("part2", String.class);
         assertEquals(25272L, method.invoke(null, String.format("src/main/resources/input%d_1eg.txt", day)));
-    }
-
-    @Test
-    public void part2Sol() throws Exception {
-        Method method = Class.forName("Solutions.Day" + day).getMethod("part2", String.class);
-        assertEquals(2497445L, method.invoke(null, String.format("src/main/resources/input%d_1.txt", day)));
     }
 }
